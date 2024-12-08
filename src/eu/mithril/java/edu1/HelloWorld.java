97344439adbe36d28ef98f04b4e52ad3b58c07e1
Marko Šutić <marko.sutic@mithril.eu>
2024-12-08 18:48:53 +0100

    Add initial implementation of HelloWorld and CircleCalculator

---
+++ src/eu/mithril/java/edu1/HelloWorld.java
@@ -0,0 +1,17 @@
+package eu.mithril.java.edu1;
+
+/**
+ * Javadoc comment
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // System.out.println("This line is not executed");
+        /*
+        System.out.println("This line is ignored by compiler");
+        System.out.println("Including this");
+        */
+    }
+
+}
---
+++ src/eu/mithril/java/edu2/CircleCalculator.java
@@ -0,0 +1,43 @@
+package eu.mithril.java.edu2;
+
+import java.util.Scanner;
+
+/*
+ * Circle Calculator Exercise
+
+ * This program calculates the area and perimeter of a circle based on user input.
+
+ * Requirements:
+ * 1. Use Scanner class to get radius from user
+ * 2. Validate that the input is:
+ *    - A valid number (not text or special characters)
+ *    - Positive (greater than 0)
+ * 3. Calculate and display:
+ *    - Area (π * r²)
+ *    - Perimeter (2 * π * r)
+ * 4. Format results to 2 decimal places
+ * 5. Include appropriate error messages for invalid input
+ * Tips:
+ * - Use Math.PI for the value of π
+ * - Use scanner.hasNextDouble() to check if input is a number
+ * - Use String.format("%.2f", value) for decimal formatting
+ */
+public class CircleCalculator {
+    public static void main(String[] args) {
+        // Create Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        // TODO: Declare necessary constants (PI)
+
+        // TODO: Implement input validation loop
+
+        // TODO: Get and validate user input
+
+        // TODO: Calculate area and perimeter
+
+        // TODO: Display formatted results
+
+        // Close scanner
+        scanner.close();
+    }
+}
