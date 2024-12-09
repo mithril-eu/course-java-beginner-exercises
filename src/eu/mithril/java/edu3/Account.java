a3206707170d5d31d0cf25026c85d78bd804b37a
Marko Šutić <marko.sutic@mithril.eu>
2024-12-09 02:01:38 +0100

    Add Account class exercise

---
+++ src/eu/mithril/java/edu3/Account.java
@@ -0,0 +1,11 @@
+package eu.mithril.java.edu3;
+
+/**
+ * The Account class represents a bank account. <br/>
+ * It provides basic banking operations like deposit and withdrawal. <br/>
+ * Each account has a unique ID and maintains its balance in EUR. <br/>
+ */
+public class Account {
+
+
+}
---
+++ src/eu/mithril/java/edu3/AccountExercise.java
@@ -0,0 +1,39 @@
+package eu.mithril.java.edu3;
+
+/*
+ * Banking Exercise
+ *
+ * This program demonstrates working with Account objects by:
+ * 1. Creating two Account instances with IDs and initial balances
+ * 2. Performing a deposit on one account
+ * 3. Performing a withdrawal from another account
+ * 4. Printing final balances of both accounts with their IDs and currency
+ *
+ * Requirements:
+ * 1. Create two Account objects with different IDs and initial balances
+ * 2. Deposit some money to the first account
+ * 3. Withdraw some money from the second account
+ * 4. Print the ID and balance (with currency) of both accounts after operations
+ *
+ * Example output:
+ * Account ID 1 balance: 150.00 EUR
+ * Account ID 2 balance: 70.00 EUR
+ *
+ * Notice:
+ * - Use Account.getCurrency() to get the currency for display
+ * - Format the balance to exactly 2 decimal places
+ */
+public class AccountExercise {
+
+    public static void main(String[] args) {
+        // TODO: Create two Account objects with IDs and initial balances
+
+        // TODO: Make a deposit to the first account
+
+        // TODO: Make a withdrawal from the second account
+
+        // TODO: Print both account balances with IDs and currency
+
+    }
+
+}
